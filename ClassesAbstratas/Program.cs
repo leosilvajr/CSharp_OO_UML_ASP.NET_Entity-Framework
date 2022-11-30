5e25b20e731c877ef9dc4ead5d25d9d72b8f2a2e
Leo <leosilvatanabi@gmail.com>
2022-11-30 16:03:13 -0300

    Classes Abstratas , [HerancaPolimorfismoExercicio]

---
+++ ClassesAbstratas/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassesAbstratas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassesAbstratas
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Eu na consigo Instanciar a classe Account, porem consigo criar uma lista.;
+            List<Account> list = new List<Account>();
+
+            list.Add(new SavingsAccount(1001, "ALEX", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "MARIA", 500.0, 400.0));
+            list.Add(new SavingsAccount(1003, "BOB", 500.0, 0.01));
+            list.Add(new BusinessAccount(1004, "ANNA", 500.0, 500.0));
+
+            double sum = 0.0;
+            foreach ( Account acc in  list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("TOTAL BALANCE: "+ sum.ToString("F2",CultureInfo.InvariantCulture));
+
+            //Metodo de forma Polimorfica
+            foreach (Account acc in list)
+            {
+                acc.WithDraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Update balance for account: "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F", CultureInfo.InvariantCulture)
+                    );
+            }
+
+        }
+    }
+}
---
+++ HerancaPolimorfismoExercicio/Program.cs
@@ -0,0 +1,56 @@
+﻿using HerancaPolimorfismoExercicio.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerancaPolimorfismoExercicio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("Enter the number of employyes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"EMPLOYEE #{i} data: ");
+                Console.Write("Outsourced: [Y/N]"); //Tercerizado ?
+                char ch = char.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'Y' || ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS: ");
+            foreach (Employee emp in list)
+            {
+                Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2"),CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
